f4d0ffd20c12d99650ca7cc9031e1b8af443b83b
anirudh-qual <63230543+anirudh-qual@users.noreply.github.com>
2021-09-18 23:37:02 +0530

    Add files via upload

---
+++ Devathon_Project/ws3/student/forms.py
@@ -0,0 +1,15 @@
+from .models import Student
+from django import forms
+from django.contrib.auth.models import User
+
+
+
+class StudentForm(forms.ModelForm):
+    class Meta:
+
+        model = Student
+
+        fields = '__all__'
+
+
+
